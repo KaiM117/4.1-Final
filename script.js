99f619aa440fa7178f397c43c18ad9e5ca2527fb
Malakai <malakai.m177@gmail.com>
2025-09-03 04:07:54 -0700

    second commit

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-/// i need to add a loading state to the btn 
-/// i want to make the cinema img dissapear and replace it with a filter toggle and then the display of the 6 movies from the backend
+
 
 /* api  https://www.omdbapi.com/?i=tt3896198&apikey=8804eea5&s= */
 
-document.getElementById('searchButton').addEventListener('click', function() {
+/*document.getElementById('searchButton').addEventListener('click', function() {
     const query = document.getElementById('searchInput').value;
     searchAPI(query);
-});
+});*/
 
-async function searchAPI(query) {
-    const movies = await fetch (`https://www.omdbapi.com/?i=tt3896198&apikey=8804eea5&s=${query}`);
-    const moviesData = await movies.slice(0, 6).json()
-}
-console.log(searchAPI())
+async function searchAPI() {
+  /*const movies = await fetch (`https://www.omdbapi.com/?i=tt3896198&apikey=8804eea5&s=${query}`);*/
+  const movies = await fetch (`https://www.omdbapi.com/?i=tt3896198&apikey=8804eea5&s=big_hero_6`);
+  const moviesData = await movies.json()
+  const limitedMovies = moviesData.slice(0,2)
+} 
+
+/*console.log(searchAPI())
 function movieHTML(movie, series){
     return `<div class="film__wrapper">
             <img
@@ -25,4 +27,4 @@ function movieHTML(movie, series){
             <p class="genre">Genre(s):${movie.genre}</p>
             <p class="imdb__rating">Rating:${movie.imdbRating}</p>
           </div>`
-}
+}*/
