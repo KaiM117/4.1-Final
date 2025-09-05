54e1234120d53e8c1d951768c5133e7e709a27e3
Malakai <malakai.m177@gmail.com>
2025-09-05 00:10:01 -0700

    sixth commit

--- script.js
+++ script.js
@@ -3,24 +3,24 @@
 async function searchAPI() {
   /*const movies = await fetch (`https://www.omdbapi.com/?i=tt3896198&apikey=8804eea5&s=${query}`);*/
   const movies = await fetch(
-    `https://www.omdbapi.com/?i=tt3896198&apikey=8804eea5&s=big_hero_6`
+    `https://www.omdbapi.com/?i=tt3896198&apikey=8804eea5&i=big_hero_6`
   );
   const moviesData = await movies.json();
-  const limitedMovies = moviesData.Search.slice(0, 3);
+  const limitedMovies = moviesData.Search.Slice(0, 3);
   const movieListEle = document.querySelector("#searchResults")
 
   movieListEle.innerHTML = limitedMovies.map(
     (movie) => `<div class="film__wrapper">
             <img
               class="film__img"
-              src="${Poster}"
+              src="${limitedMovies.Search.Poster}"
               alt=""
             />
-            <h3 class="film__name">${movie.Title}</h3>
-            <p class="media__type">${Type}</p>
-            <p class="release__date">Release Date:${movie.Released}</p>
-            <p class="genre">Genre(s):${movie.Genre}</p>
-            <p class="imdb__rating">Rating:${movie.imdbRating}</p>
+            <h3 class="film__name">${limitedMovies.Search.Title}</h3>
+            <p class="media__type">${limitedMovies.Search.Type}</p>
+            <p class="release__date">Release Date:${limitedMovies.Search.Released}</p>
+            <p class="genre">Genre(s):${limitedMovies.Search.Genre}</p>
+            <p class="imdb__rating">Rating:${limitedMovies.Search.imdbRating}</p>
           </div>`).join("")
   console.log(limitedMovies);
 }
