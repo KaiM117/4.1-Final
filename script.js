6de716611f26a79039eef269bd854af0f2e15f2f
Malakai <malakai.m177@gmail.com>
2025-09-04 23:44:38 -0700

    fourth commit

--- script.js
+++ script.js
@@ -1,53 +1,26 @@
 /* api  https://www.omdbapi.com/?i=tt3896198&apikey=8804eea5&s= */
 
-/*document.getElementById('searchButton').addEventListener('click', function() {
-    const query = document.getElementById('searchInput').value;
-    searchAPI(query);
-});*/
-
 async function searchAPI() {
   /*const movies = await fetch (`https://www.omdbapi.com/?i=tt3896198&apikey=8804eea5&s=${query}`);*/
   const movies = await fetch(
     `https://www.omdbapi.com/?i=tt3896198&apikey=8804eea5&s=big_hero_6`
   );
   const moviesData = await movies.json();
-  const limitedMovies = moviesData.Search.slice(0, 2);
+  const limitedMovies = moviesData.Search.slice(0, 3);
+  const movieListEle = document.querySelector("#searchResults")
+
+  movieListEle.innerHTML = limitedMovies.map(
+    (movie) => `<div class="film__wrapper">
+            <img
+              class="film__img"
+              src=""
+              alt=""
+            />
+            <h3 class="film__name">${movie.Title}</h3>
+            <p class="media__type"></p>
+            <p class="release__date">Release Date:${movie.Released}</p>
+            <p class="genre">Genre(s):${movie.genre}</p>
+            <p class="imdb__rating">Rating:${movie.imdbRating}</p>
+          </div>`).join("")
   console.log(limitedMovies);
 }
-
-function movieHTML(movie) {
-  return `<div class="film__wrapper">
-            <img
-              class="film__img"
-              src="${movie.Poster}"
-              alt=""
-            />
-            <h3 class="film__name">${movie.Title}</h3>
-            <p class="release__date">Release Date:${movie.Released}</p>
-            <p class="genre">Genre(s):${movie.genre}</p>
-            <p class="imdb__rating">Rating:${movie.imdbRating}</p>
-          </div>`;
-}
-
-function displayResults(data){ ///fowtfiwwtsn
-  const resultsDiv = document.getElementById("searchResults");
-  resultsDiv.innerHTML = "";
-
-  data.forEach((item) => {
-    const resultItem = document.createElement("div");
-    resultItem.innerHTML = `<div class="film__wrapper">
-            <img
-              class="film__img"
-              src="${movie.Poster}"
-              alt=""
-            />
-            <h3 class="film__name">${movie.Title}</h3>
-            <p class="release__date">Release Date:${movie.Released}</p>
-            <p class="genre">Genre(s):${movie.genre}</p>
-            <p class="imdb__rating">Rating:${movie.imdbRating}</p>
-          </div>`;
-    resultsDiv.appendChild(resultItem);
-  });
-  console.log(displayResults(limitedMovies))
-}
-;
